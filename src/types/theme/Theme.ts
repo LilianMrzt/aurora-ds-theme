3b828ae6588bac7184fbdf85677d9f85b14099f6
Lilian Marzet <lilian.marzet123@gmail.com>
2026-01-18 23:00:20 +0100

    fix: typescript checks

--- src/types/theme/Theme.ts
+++ src/types/theme/Theme.ts
@@ -2,14 +2,10 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ThemeRegistry {}
 
-/** @internal */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type _InferredTheme = ThemeRegistry extends { theme: infer T } ? T : Record<string, any>
-
 /**
  * Internal theme type used by the library. DO NOT USE directly.
  * Define your own theme type and register it via ThemeRegistry.
  * @internal
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type _InternalTheme<T = _InferredTheme> = T extends Record<string, any> ? T : Record<string, any>
+export type _InternalTheme = ThemeRegistry extends { theme: infer T } ? T : Record<string, any>
